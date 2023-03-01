27f030567bdff3ec0116a12c718afdca8bf5ee51
DavidSchurer <122594694+DavidSchurer@users.noreply.github.com>
2023-03-01 13:05:26 -0800

    Update assignment4.cpp

--- assignment4.cpp
+++ assignment4.cpp
@@ -4,294 +4,294 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+#include <random>
 #include <cstdlib>
 #include <ctime>
-#include <cassert>
-#include <random>
 #include "skiplist.h"
 using namespace std;
 
-// The test function singleLevelSkipListTest will test the Add, Remove, and Contains methods
-// on a SkipList that has just a single level
-void singleLevelSkipListTest() {
+// SkipList constructor will initialize all of the levels in the SkipList as well as the rearGuards and frontGuards for each level and link them together
+SkipList::SkipList(int depth) {
 
-    SkipList singleLevelSkipList(1);
+    srand(time(NULL));
 
-    singleLevelSkipList.Add(84);
-    cout << "After adding 84" << endl;
-    cout << singleLevelSkipList << endl;
+    // Allocating memory for two arrays of SNode pointers that will represent the frontGuards and rearGuards in the SkipList
+    // Each array has a max size of the depth of the SkipList
+    frontGuards = new SNode * [depth];
+    rearGuards = new SNode * [depth];
 
-    singleLevelSkipList.Add(66);
-    cout << "After adding 66" << endl;
-    cout << singleLevelSkipList << endl;
+    // The frontGuards and rearGuards are initialized for the first level of the SkipList, their data stores the min and max values for integers
+    frontGuards[0] = new SNode(INT_MIN);
+    rearGuards[0] = new SNode(INT_MAX);
 
-    singleLevelSkipList.Add(21);
-    cout << "After adding 21" << endl;
-    cout << singleLevelSkipList << endl;
+    // The frontGuards and rearGuards for the first level are linked together
+    frontGuards[0]->next = rearGuards[0];
+    rearGuards[0]->prev = frontGuards[0];
 
-    singleLevelSkipList.Add(-5);
-    cout << "After adding -5" << endl;
-    cout << singleLevelSkipList << endl;
+    // This for loop initializes all of the levels of the SkipList with each front and read guard at each level
+    for (int i = 1; i < depth; i++) {
 
-    singleLevelSkipList.Add(-20);
-    cout << "After adding -20" << endl;
-    cout << singleLevelSkipList << endl;
+        frontGuards[i] = new SNode(INT_MIN);
+        rearGuards[i] = new SNode(INT_MAX);
 
-    singleLevelSkipList.Remove(66);
-    cout << "After removing 66" << endl;
-    cout << singleLevelSkipList << endl;
+        frontGuards[i]->next = rearGuards[i];
+        rearGuards[i]->prev = frontGuards[i];
 
-    singleLevelSkipList.Remove(-5);
-    cout << "After removing -5" << endl;
-    cout << singleLevelSkipList << endl;
+        // While the level is above 0 in the SkipList, each front and rear guard will be connected
+        // to the front and rear guards above and below them
+        frontGuards[i]->downLevel = frontGuards[i - 1];
+        rearGuards[i]->downLevel = rearGuards[i - 1];
+        frontGuards[i - 1]->upLevel = frontGuards[i];
+        rearGuards[i - 1]->upLevel = rearGuards[i];
 
-    assert(singleLevelSkipList.Contains(84) == true);
-    cout << "The skiplist contains 84" << endl;
+    }
+    // The vector levels will store the SNode pointers for each level
+    levels = vector<SNode*>(depth);
 
-    assert(singleLevelSkipList.Contains(21) == true);
-    cout << "The skiplist contains 21" << endl;
+    for (int i = 0; i < depth; i++) {
+        levels[i] = frontGuards[i];
+    }
 
-    assert(singleLevelSkipList.Add(84) == false);
-    cout << "Cannot add a duplicate of 84 into the skiplist" << endl;
+    this->depth = depth;
+}
 
-    assert(singleLevelSkipList.Add(21) == false);
-    cout << "Cannot add a duplicate of 21 into the skiplist" << endl;
+// SNode constructor initializes a new node with its given int data
+// and sets all of its pointers to null
+SNode::SNode(int data) {
+
+    this->data = data;
+    this->next = nullptr;
+    this->prev = nullptr;
+    this->upLevel = nullptr;
+    this->downLevel = nullptr;
 
 }
 
-// The test function twoLevelSkipListTest will test the Add, Remove, and Contains methods
-// on a SkipList that has 2 levels
-void twoLevelSkipListTest() {
+// The SkipList destructor frees up any dynamically allocated memory
+// by deleting all of the nodes in the SkipList
+SkipList::~SkipList() {
 
-    SkipList smallSkipList(2);
+    // Start at the first level in the SkipList and iterates until the highest level has been reached
+    for (int i = 0; i < depth; i++) {
 
-    smallSkipList.Add(898);
-    cout << "After adding 898" << endl;
-    cout << smallSkipList << endl;
+        // Start at the node that the frontGuard points to
+        SNode* currentNode = frontGuards[i]->next;
 
-    smallSkipList.Add(-24);
-    cout << "After adding -24" << endl;
-    cout << smallSkipList << endl;
-
-    smallSkipList.Add(57);
-    cout << "After adding 57" << endl;
-    cout << smallSkipList << endl;
-
-    smallSkipList.Add(1024);
-    cout << "After adding 1024" << endl;
-    cout << smallSkipList << endl;
-
-    smallSkipList.Add(404);
-    cout << "After adding 404" << endl;
-    cout << smallSkipList << endl;
-
-    smallSkipList.Add(-111);
-    cout << "After adding -111" << endl;
-    cout << smallSkipList << endl;
-
-    smallSkipList.Remove(1024);
-    cout << "After removing 1024" << endl;
-    cout << smallSkipList << endl;
-
-    smallSkipList.Remove(57);
-    cout << "After removing 57" << endl;
-    cout << smallSkipList << endl;
-
-    assert(smallSkipList.Contains(404) == true);
-    cout << "The skiplist contains 404" << endl;
-
-    assert(smallSkipList.Contains(-111) == true);
-    cout << "The skiplist contains -111" << endl;
-
-    assert(smallSkipList.Add(404) == false);
-    cout << "Cannot add a duplicate of 404 into the skiplist" << endl;
-
-    assert(smallSkipList.Add(-111) == false);
-    cout << "Cannot add a duplicate of -111 into the skiplist" << endl;
+        // Traverse through the list, deleting each node until rearGuard is reached
+        while (currentNode != rearGuards[i]) {
+            SNode* tempNode = currentNode;
+            currentNode = currentNode->next;
+            delete tempNode;
+        }
+        // Delete the rearGuards and frontGuards for each level
+        delete rearGuards[i];
+        delete frontGuards[i];
+    }
+    // Delete the rearGuards and frontGuards arrays
+    delete[] rearGuards;
+    delete[] frontGuards;
 
 }
 
-// The test function fiveLevelSkipListTest will test the Add, Remove, and Contains methods
-// on a SkipList that has 5 levels
-void fiveLevelSkipListTest() {
+// The add method will create a new node with the given data and insert
+// it into the SkipList, each node that is added will have a 50% chance of
+// being inserted at a higher level 
+bool SkipList::Add(int data) {
 
-    SkipList mediumSkipList(5);
+    SNode* newNode = new SNode(data);
+    SNode* nextNode = frontGuards[0]->next;
+    SNode* previousNode = frontGuards[0];
 
-    mediumSkipList.Add(-10);
-    cout << "After adding -10" << endl;
-    cout << mediumSkipList << endl;
+    int currentLevel = 0;
 
-    mediumSkipList.Add(-5);
-    cout << "After adding -5" << endl;
-    cout << mediumSkipList << endl;
+    // As long as the data in the next node is smaller than the data we want to insert
+    // keep traversing
+    while (nextNode->data < data) {
+        previousNode = nextNode;
+        nextNode = nextNode->next;
+    }
 
-    mediumSkipList.Add(5);
-    cout << "After adding 5" << endl;
-    cout << mediumSkipList << endl;
+    // If the data we want to insert is already in the SkipList, update
+    // the next node to contain that data and delete the new node
+    if (nextNode->data == data) {
 
-    mediumSkipList.Add(10);
-    cout << "After adding 10" << endl;
-    cout << mediumSkipList << endl;
+        nextNode->data = data;
+        delete newNode;
+        return false; // We return false because we did not add data (no duplicates allowed)
 
-    mediumSkipList.Add(15);
-    cout << "After adding 15" << endl;
-    cout << mediumSkipList << endl;
+    }
 
-    mediumSkipList.Add(20);
-    cout << "After adding 20" << endl;
-    cout << mediumSkipList << endl;
+    // Call the addBefore method to insert the new node before the next node at the first level
+    addBefore(newNode, nextNode);
 
-    mediumSkipList.Add(25);
-    cout << "After adding 25" << endl;
-    cout << mediumSkipList << endl;
+    // 50% chance to determine whether the node will be inserted at a higher level
+    while (alsoHigher() && currentLevel < depth - 1) {
+        currentLevel++;
 
-    mediumSkipList.Add(30);
-    cout << "After adding 30" << endl;
-    cout << mediumSkipList << endl;
+        // Move up one level
+        while (previousNode->upLevel == nullptr) {
+            previousNode = previousNode->prev;
+        }
+        previousNode = previousNode->upLevel;
 
-    mediumSkipList.Add(35);
-    cout << "After adding 35" << endl;
-    cout << mediumSkipList << endl;
+        // New node is created to be inserted in the upper level
+        SNode* newUpperNode = new SNode(data);
 
-    mediumSkipList.Add(40);
-    cout << "After adding 40" << endl;
-    cout << mediumSkipList << endl;
+        // The new node will be inserted before the next node in the upper level
+        addBefore(newUpperNode, previousNode->next);
 
-    mediumSkipList.Remove(-10);
-    cout << "After removing -10" << endl;
-    cout << mediumSkipList << endl;
+        // The new node now links to its corresponding node below it in the lower level
+        newUpperNode->downLevel = newNode;
+        newNode->upLevel = newUpperNode;
+        // The upper node is now set to new node so that we can add more levels to it
+        newNode = newUpperNode;
 
-    mediumSkipList.Remove(30);
-    cout << "After removing 30" << endl;
-    cout << mediumSkipList << endl;
+    }
+    // Returns true if the data was added
+    return true;
+}
 
-    assert(mediumSkipList.Contains(20) == true);
-    cout << "The skiplist contains 20" << endl;
+// The remove method will start at the top of the SkipList and traverse through each level checking for the data
+// to remove, it will ensure that the data is removed at each level that it is found in the SkipList
+bool SkipList::Remove(int data) {
 
-    assert(mediumSkipList.Contains(40) == true);
-    cout << "The skiplist contains 40" << endl;
+    // Initialize currentNode pointer to start at the frontGuard of the top level of the SkipList
+    SNode* currentNode = frontGuards[depth - 1];
 
-    assert(mediumSkipList.Add(20) == false);
-    cout << "Cannot add a duplicate of 20 into the skiplist" << endl;
+    // Keep iterating while the currentNode is not a nullptr
+    while (currentNode != nullptr) {
+        // If the next node's data is equal to the data we are looking for
+        if (currentNode->next->data == data) {
+            // Remove this node that contains the data we are looking for
+            SNode* deleteNode = currentNode->next;
+            currentNode->next = deleteNode->next;
+            deleteNode->next->prev = currentNode;
+            delete deleteNode;
 
-    assert(mediumSkipList.Add(40) == false);
-    cout << "Cannot add a duplicate of 40 into the skiplist" << endl;
+            // If the currentNode is not at the first level of the skipList, it will traverse down one level
+            if (currentNode->downLevel != nullptr) {
+                currentNode = currentNode->downLevel;
+                continue;
+            }
+            return true;
+
+            // If the currentNode is not at the first level and the next node's data is greater than the data to be removed, go down a level
+        }
+        else if ((currentNode->downLevel != nullptr) && (currentNode->next->data > data)) {
+            currentNode = currentNode->downLevel;
+
+            // If the currentNode is at the first level and the next node's data is greater than the data to be removed, then that data was not found in the SkipList
+        }
+        else if ((currentNode->downLevel == nullptr) && (currentNode->next->data > data)) {
+            return false;
+
+            // Else the current node will keep traversing to the next node in the current level
+        }
+        else {
+            currentNode = currentNode->next;
+        }
+
+    }
+    return false;
+}
+
+// The contains method starts at the top of the SkipList and traverses through each level checking
+// if the data that we are looking for is in the SkipList
+bool SkipList::Contains(int data) {
+
+    // Initialize currentNode pointer to start at the frontGuard of the top level of the SkipList
+    SNode* currentNode = frontGuards[depth - 1];
+
+    // Keep iterating while the currentNode is not a nullptr
+    while (currentNode != nullptr) {
+        // If the next node's data is equal to the data we are looking for
+        if (currentNode->next->data == data) {
+            return true;
+
+            // If the currentNode is not at the first level of the skipList, it will traverse down one level
+            if (currentNode->downLevel != nullptr) {
+                currentNode = currentNode->downLevel;
+                continue;
+            }
+            return true;
+
+            // If the currentNode is not at the first level and the next node's data is greater than the data we are looking for, go down a level
+        }
+        else if ((currentNode->downLevel != nullptr) && (currentNode->next->data > data)) {
+            currentNode = currentNode->downLevel;
+
+            // If the currentNode is at the first level and the next node's data is greater than the data we are looking for, then that data was not found in the SkipList
+        }
+        else if ((currentNode->downLevel == nullptr) && (currentNode->next->data > data)) {
+            return false;
+
+            // Else the current node will keep traversing to the next node in the current level
+        }
+        else {
+            currentNode = currentNode->next;
+        }
+
+    }
+    return false;
 
 }
 
-// The test function tenLevelSkipListTest will test the Add, Remove, and Contains methods
-// on a SkipList that has 10 levels
-void tenLevelSkipListTest() {
+// The addBefore method inserts a new node before the nextNode at the current
+// level of the SkipList
+void SkipList::addBefore(SNode* newNode, SNode* nextNode) {
 
-    SkipList largeSkipList(10);
+    SNode* previousNode = nextNode->prev;
 
-    largeSkipList.Add(42);
-    cout << "After adding 42" << endl;
-    cout << largeSkipList << endl;
+    // This while loop iterates backwards until it reaches the first node in the list
+    // whose data is less than or equal to the data of the new node that will be inserted
+    while ((previousNode != nullptr) && (previousNode->data > newNode->data)) {
+        nextNode = previousNode;
+        previousNode = nextNode->prev;
+    }
 
-    largeSkipList.Add(6823);
-    cout << "After adding 6823" << endl;
-    cout << largeSkipList << endl;
+    // The new node's pointers are set so that it is linked with the previous node and next node
+    newNode->next = nextNode;
+    newNode->prev = previousNode;
 
-    largeSkipList.Add(4122);
-    cout << "After adding 4122" << endl;
-    cout << largeSkipList << endl;
-
-    largeSkipList.Add(601);
-    cout << "After adding 601" << endl;
-    cout << largeSkipList << endl;
-
-    largeSkipList.Add(394);
-    cout << "After adding 394" << endl;
-    cout << largeSkipList << endl;
-
-    largeSkipList.Add(9999);
-    cout << "After adding 9999" << endl;
-    cout << largeSkipList << endl;
-
-    largeSkipList.Add (-295);
-    cout << "After adding -295" << endl;
-    cout << largeSkipList << endl;
-
-    largeSkipList.Add(-5293);
-    cout << "After adding -5293" << endl;
-    cout << largeSkipList << endl;
-
-    largeSkipList.Add(71);
-    cout << "After adding 71" << endl;
-    cout << largeSkipList << endl;
-
-    largeSkipList.Add(122);
-    cout << "After adding 122" << endl;
-    cout << largeSkipList << endl;
-
-    largeSkipList.Add(42);
-    cout << "After adding 42" << endl;
-    cout << largeSkipList << endl;
-
-    largeSkipList.Add(318);
-    cout << "After adding 318" << endl;
-    cout << largeSkipList << endl;
-
-    largeSkipList.Add(696);
-    cout << "After adding 696" << endl;
-    cout << largeSkipList << endl;
-
-    largeSkipList.Add(888);
-    cout << "After adding 888" << endl;
-    cout << largeSkipList << endl;
-
-    largeSkipList.Add(1000);
-    cout << "After adding 1000" << endl;
-    cout << largeSkipList << endl;
-
-    largeSkipList.Remove(394);
-    cout << "After removing 394" << endl;
-    cout << largeSkipList << endl;
-
-    largeSkipList.Remove(4122);
-    cout << "After removing 4122" << endl;
-    cout << largeSkipList << endl;
-
-    assert(largeSkipList.Contains(1000) == true);
-    cout << "The skiplist contains 1000" << endl;
-
-    assert(largeSkipList.Contains(-5293) == true);
-    cout << "The skiplist contains -5293" << endl;
-
-    assert(largeSkipList.Add(1000) == false);
-    cout << "Cannot add a duplicate of 1000 into the skiplist" << endl;
-
-    assert(largeSkipList.Add(-5293) == false);
-    cout << "Cannot add a duplicate of -5293 into the skiplist" << endl;
+    // The next node's previous pointer is set to new node
+    nextNode->prev = newNode;
+    // the previous node's next pointer is set to new node
+    previousNode->next = newNode;
 
 }
 
-int main() {
+// The alsoHigher method returns true 50% of the time, it uses
+// rand() to generate a random number between 0 and 1 and returns true
+// if the number it receives is 0
+bool SkipList::alsoHigher() const {
 
-    cout << "############### TEST 1: SINGLE LEVEL SKIPLIST ###############" << endl;
-    singleLevelSkipListTest();
-    cout << "#############################################################" << endl;
+    return (rand() % 2 == 0);
+}
 
-    cout << endl;
+// The overloaded output operator for the SkipList will print out the contents of the SkipList to the
+// output stream including each depth level, front and rear guards, and the items in each level of
+// the Skiplist
+ostream& operator<<(ostream& os, const SkipList& list) {
 
-    cout << "############### TEST 2: TWO LEVEL SKIPLIST ###############" << endl;
-    twoLevelSkipListTest();
-    cout << "##########################################################" << endl;
+    // Loop through each level in the SkipList, starting at the top level
+    for (int i = list.depth - 1; i >= 0; i--) {
+        // Sets the current pointer to point to the next node after the frontGuard
+        SNode* current = list.frontGuards[i]->next;
 
-    cout << endl;
+        // Print the level of the SkipList and the front guard's data
+        os << "Level: " << i << " -- ";
+        os << list.frontGuards[i]->data << ", ";
 
-    cout << "############### TEST 3: FIVE LEVEL SKIPLIST ###############" << endl;
-    fiveLevelSkipListTest();
-    cout << "###########################################################" << endl;
+        // Traverses through the nodes on each level and prints each node's data
+        while (current != list.rearGuards[i]) {
+            os << current->data << ", ";
+            current = current->next;
+        }
 
-    cout << endl;
-
-    cout << "############### TEST 4: TEN LEVEL SKIPLIST ###############" << endl;
-    tenLevelSkipListTest();
-    cout << "##########################################################" << endl;
-
-    cout << endl;
-
-    return 0;
+        // Print the data for the rear guard and move to the next line
+        os << list.rearGuards[i]->data;
+        cout << endl;
+    }
+    // Returns the output stream object
+    return os;
 }
